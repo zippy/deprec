67df8dc10b052706cfa2e094d513075c4720d66f
Eric Harris-Braun <eric@harris-braun.com>
2014-08-25 12:48:47 -0400

    update passenger version

--- lib/deprec/recipes/passenger.rb
+++ lib/deprec/recipes/passenger.rb
@@ -1,9 +1,9 @@
 # Copyright 2006-2008 by Mike Bailey. All rights reserved.
-Capistrano::Configuration.instance(:must_exist).load do 
-  namespace :deprec do 
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :deprec do
     namespace :passenger do
 
-      set :passenger_version, '4.0.2'    
+      set :passenger_version, '4.0.42'
       set(:passenger_root) { capture("passenger-config --root").chomp }
       set(:passenger_ruby) { capture("which ruby").chomp }
 
@@ -11,7 +11,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       set(:passenger_document_root) { "#{current_path}/public" }
       set :passenger_rails_allow_mod_rewrite, 'off'
       set :passenger_log_level, 0
-      set(:passenger_log_dir) { "#{shared_path}/log"} 
+      set(:passenger_log_dir) { "#{shared_path}/log"}
       set :passenger_user_switching, 'on'
       set :passenger_default_user, 'nobody'
       set :passenger_max_pool_size, 6
@@ -36,19 +36,19 @@ Capistrano::Configuration.instance(:must_exist).load do
         run "#{sudo} passenger-install-apache2-module _#{passenger_version}_ --auto"
         config_system
       end
-      
+
       # Install dependencies for Passenger
       task :install_deps, :roles => :app do
         apt.install( {:base => %w(libcurl4-openssl-dev apache2-mpm-prefork apache2-prefork-dev libapr1-dev libaprutil1-dev rsync)}, :stable )
       end
-      
+
       SYSTEM_CONFIG_FILES[:passenger] = [
 
         {:template => 'passenger.load.erb',
           :path => '/etc/apache2/mods-available/passenger.load',
           :mode => 0755,
           :owner => 'root:root'},
-          
+
         {:template => 'passenger.conf.erb',
           :path => '/etc/apache2/mods-available/passenger.conf',
           :mode => 0755,
@@ -67,22 +67,22 @@ Capistrano::Configuration.instance(:must_exist).load do
           :path => 'apache_vhost_disabled',
           :mode => 0755,
           :owner => 'root:root'},
-          
+
         {:template => 'logrotate.conf.erb',
-         :path => "logrotate.conf", 
+         :path => "logrotate.conf",
          :mode => 0644,
          :owner => 'root:root'}
 
       ]
-       
+
       desc "Generate Passenger apache configs (system & project level)."
       task :config_gen do
-        # config_gen_system 
+        # config_gen_system
         config_gen_project
       end
 
       # desc "Generate Passenger apache configs (system level) from template."
-      # We can't generate passenger configs because we need to contact remote system 
+      # We can't generate passenger configs because we need to contact remote system
       # to find out gem location & ruby version.
       task :config_gen_system do
         #puts "We don't generate local Passenger system files"
@@ -101,7 +101,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "Push Passenger config files (system & project level) to server"
       task :config, :roles => :app do
         config_system
-        config_project  
+        config_project
       end
 
       desc "Push Passenger configs (system level) to server"
@@ -117,26 +117,26 @@ Capistrano::Configuration.instance(:must_exist).load do
         activate_project
         symlink_logrotate_config
       end
-      
+
       task :symlink_logrotate_config, :roles => :app do
         sudo "ln -sf #{deploy_to}/passenger/logrotate.conf /etc/logrotate.d/passenger-#{application}"
       end
-      
+
       # Passenger runs Rails as the owner of this file.
       task :set_owner_of_environment_rb, :roles => :app do
         sudo "chown #{app_user} #{current_path}/config/environment.rb"
       end
-      
+
       task :symlink_apache_vhost, :roles => :app do
         sudo "ln -sf #{deploy_to}/passenger/apache_vhost #{apache_vhost_dir}/#{application}"
         sudo "ln -sf #{deploy_to}/passenger/apache_vhost_disabled #{apache_vhost_dir}/#{application}_disabled"
       end
-      
+
       task :activate, :roles => :app do
         activate_system
         activate_project
       end
-      
+
       task :activate_system, :roles => :app do
         sudo "a2enmod passenger"
         if passenger_use_mod_rewrite_for_disable
@@ -144,12 +144,12 @@ Capistrano::Configuration.instance(:must_exist).load do
         end
         top.deprec.web.reload
       end
-      
+
       task :activate_project, :roles => :app do
         sudo "a2ensite #{application}"
         top.deprec.web.reload
       end
-      
+
       task :deactivate do
         puts
         puts "******************************************************************"
@@ -165,20 +165,20 @@ Capistrano::Configuration.instance(:must_exist).load do
         puts "******************************************************************"
         puts
       end
-      
+
       task :deactivate_system, :roles => :app do
         sudo "a2dismod passenger"
         top.deprec.web.reload
       end
-      
+
       task :deactivate_project, :roles => :app do
         sudo "a2dissite #{application}"
         top.deprec.web.reload
       end
-      
+
       desc <<-DESC
         Present a maintenance page to visitors. Disables your application's web \
-        interface by writing a "maintenance.html" file to each web server. 
+        interface by writing a "maintenance.html" file to each web server.
 
         By default, the maintenance page will just say the site is down for \
         "maintenance", and will be back "shortly", but you can customize the \
@@ -205,7 +205,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           top.deprec.web.reload
         end
       end
-      
+
       desc <<-DESC
         Makes the application web-accessible again. Removes the \
         "maintenance.html" page generated by deprec:app:disable, which (if your \
@@ -225,13 +225,13 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :restart, :roles => :app do
         run "#{sudo} touch #{current_path}/tmp/restart.txt"
       end
-      
+
       desc "Restart Apache"
       task :restart_apache, :roles => :passenger do
         run "#{sudo} /etc/init.d/apache2 restart"
       end
-      
+
     end
-    
+
   end
 end
