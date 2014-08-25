b1151ef95531c9fd36e273a934e7d6df3492c69d
Eric Harris-Braun <eric@harris-braun.com>
2014-08-25 11:13:31 -0400

    updated for ruby 2.0.0-p481

--- lib/deprec/recipes/mri.rb
+++ lib/deprec/recipes/mri.rb
@@ -1,9 +1,9 @@
 # Copyright 2006-2008 by Mike Bailey. All rights reserved.
-Capistrano::Configuration.instance(:must_exist).load do 
+Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :deprec do
     namespace :mri do
-            
+
       SRC_PACKAGES['ruby-1.8.7-p330'] = {
         :md5sum => "50a49edb787211598d08e756e733e42e  ruby-1.8.7-p330.tar.gz",
         :url => "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p330.tar.gz",
@@ -12,7 +12,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       }
 
       SRC_PACKAGES['ruby-1.9.2-p180'] = {
-        :md5sum => "0d6953820c9918820dd916e79f4bfde8  ruby-1.9.2-p180.tar.gz", 
+        :md5sum => "0d6953820c9918820dd916e79f4bfde8  ruby-1.9.2-p180.tar.gz",
         :url => "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz",
         :deps => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline-gplv2-dev),
         :configure => "./configure",
@@ -20,24 +20,23 @@ Capistrano::Configuration.instance(:must_exist).load do
       }
 
       SRC_PACKAGES['ruby-1.9.3-p194'] = {
-        :md5sum => "bc0c715c69da4d1d8bd57069c19f6c0e  ruby-1.9.3-p194.tar.gz", 
+        :md5sum => "bc0c715c69da4d1d8bd57069c19f6c0e  ruby-1.9.3-p194.tar.gz",
         :url => "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz",
         :deps => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline-gplv2-dev libyaml-dev),
         :configure => "./configure",
         :post_install => 'sudo gem update --system'
       }
 
-      SRC_PACKAGES['ruby-2.0.0-p195'] = {
-        :md5sum => "0672e5af309ae99d1703d0e96eff8ea5  ruby-2.0.0-p195.tar.gz", 
-        :url => "http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p195.tar.gz",
+      SRC_PACKAGES['ruby-2.0.0-p481.tar.gz'] = {
+        :md5sum => "3913e0ad6cc572b7358e4c6a8c4b2551  ruby-2.0.0-p481.tar.gz",
+        :url => "http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz",
         :deps => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline-gplv2-dev libyaml-dev),
         :configure => "./configure",
         :post_install => 'sudo gem update --system'
       }
 
-
       src_package_options = SRC_PACKAGES.keys.select{|k| k.match /^ruby-\d\.\d\.\d/ }
-      set(:mri_src_package) { 
+      set(:mri_src_package) {
         puts "Select mri_src_package from list:"
         Capistrano::CLI.ui.choose do |menu|
           menu.choices(*src_package_options)
@@ -49,8 +48,8 @@ Capistrano::Configuration.instance(:must_exist).load do
         deprec2.download_src(SRC_PACKAGES[mri_src_package])
         deprec2.install_from_src(SRC_PACKAGES[mri_src_package])
       end
-      
+
     end
   end
-  
+
 end
